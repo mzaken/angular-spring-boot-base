231b984057081483dd5c1b21a2c189f915a5d37b
Maor <Maor@DESKTOP-8SOPUN2>
2021-09-28 10:22:45 +0300

    Initial commit.

---
+++ spring-boot-angular-base/src/main/java/com/mzaken/springbootangularbase/SpringBootAngularBaseApplication.java
@@ -0,0 +1,13 @@
+package com.mzaken.springbootangularbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAngularBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAngularBaseApplication.class, args);
+	}
+
+}
---
+++ spring-boot-angular-base/src/test/java/com/mzaken/springbootangularbase/SpringBootAngularBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mzaken.springbootangularbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAngularBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
