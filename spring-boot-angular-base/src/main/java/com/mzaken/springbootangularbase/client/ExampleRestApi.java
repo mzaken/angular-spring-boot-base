50985b439686e6beb6e25eda662c13eabe0ee1e6
Maor <Maor@DESKTOP-8SOPUN2>
2021-09-28 19:18:35 +0300

    Jwt functionality mid-work.

---
+++ spring-boot-angular-base/src/main/java/com/mzaken/springbootangularbase/client/ExampleRestApi.java
@@ -0,0 +1,13 @@
+package com.mzaken.springbootangularbase.client;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ExampleRestApi {
+
+	@GetMapping(value = "/hello")
+	public String hello() {
+		return "Hello World!";
+	}
+}
---
+++ spring-boot-angular-base/src/main/java/com/mzaken/springbootangularbase/client/rest/Endpoints.java
@@ -0,0 +1,7 @@
+package com.mzaken.springbootangularbase.client.rest;
+
+public class Endpoints {
+	
+	public static final String API_URL = "/api";
+	public static final String AUTH = "/auth";
+}
